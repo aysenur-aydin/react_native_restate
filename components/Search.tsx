4fa364791af992f4f7cae79349c218d8d9d5e166
aysenur-aydin <aydin.aysenur016@gmail.com>
2025-02-16 16:42:33 +0300

    feat(home): home screen database connection complete

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,4 +1,4 @@
-import {View, Text, Image, TextInput, TouchableOpacity} from 'react-native'
+import {View, Image, TextInput, TouchableOpacity} from 'react-native'
 import React, {useState} from 'react'
 import {router, useLocalSearchParams, usePathname} from "expo-router";
 import icons from "@/constants/icons";
@@ -20,7 +20,7 @@ const Search = () => {
         <View className="flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2">
            <View className="flex-1 flex flex-row items-center justify-start z-50">
                 <Image source={icons.search} className="size-5" />
-               <TextInput value={search} onChangeText={setSearch} placeholder="Search for anything" className="text-sm font-rubik text-black-300 ml-2 flex-1"/>
+               <TextInput value={search} onChangeText={handleSearch} placeholder="Search for anything" className="text-sm font-rubik text-black-300 ml-2 flex-1"/>
            </View>
             <TouchableOpacity>
                 <Image source={icons.filter} className="size-5" />
